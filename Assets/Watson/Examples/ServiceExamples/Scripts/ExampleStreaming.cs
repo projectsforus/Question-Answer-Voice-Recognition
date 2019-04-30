67312e9715787bfe13aacd42b710efd039708a30
Amir Ahmed <amirdarrag2013@gmail.com>
2019-04-30 17:33:00 +0200

    V1
    Co-Authored-By: Amir Ahmed <amirdarrag2013@gmail.com>

---
+++ Assets/Watson/Examples/ServiceExamples/Scripts/ExampleStreaming.cs
@@ -0,0 +1,436 @@
+﻿/**
+* Copyright 2015 IBM Corp. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*
+*/
+#pragma warning disable 0649
+
+using UnityEngine;
+using System.Collections;
+using IBM.Watson.DeveloperCloud.Logging;
+using IBM.Watson.DeveloperCloud.Services.SpeechToText.v1;
+using IBM.Watson.DeveloperCloud.Utilities;
+using IBM.Watson.DeveloperCloud.DataTypes;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class ExampleStreaming : MonoBehaviour
+{
+    #region PLEASE SET THESE VARIABLES IN THE INSPECTOR
+    [Space(10)]
+    [Tooltip("The service URL (optional). This defaults to \"https://stream.watsonplatform.net/speech-to-text/api\"")]
+    [SerializeField]
+    private string _serviceUrl;
+    public Text qesNO;
+    public Text questionTxt;
+    [Tooltip("Text field to display the results of streaming.")]
+    public Text ResultsField;
+    public Text CorrectAns;
+    public Text AnswerCase;
+    [Header("IAM Authentication")]
+    [Tooltip("The IAM apikey.")]
+    [SerializeField]
+    private string _iamApikey;
+
+    [Header("Parameters")]
+    // https://www.ibm.com/watson/developercloud/speech-to-text/api/v1/curl.html?curl#get-model
+    [Tooltip("The Model to use. This defaults to en-US_BroadbandModel")]
+    [SerializeField]
+    private string _recognizeModel;
+    #endregion
+
+    public float textSpeed;
+    WaitForSeconds delay = new WaitForSeconds(5.0f);
+    public static bool IsAnswer;
+    private bool final;
+    private string text;
+    private int index;
+    private int _recordingRoutine = 0;
+    private string _microphoneID = null;
+    private AudioClip _recording = null;
+    private int _recordingBufferSize = 1;
+    private int _recordingHZ = 22050;
+    private string[] Questions = { "Which was best for his initial drug therapy?",
+                                   "Which is the best modification to make to his treatment regimen now?",
+                                   "if the patient is cachectic, what is the best management to overcome this problem?",
+                                   "If the patient has bad socioeconomic status, and has active \n abuse problems and tics. Which drug is the best drug of choice?",
+                                   "So Do you think Norepinephrine Reuptake Inhibitor can be considered \n as 1st line of treatment in this case?"};
+
+    private string[] Answers = {"Methylphenidate",
+                                "Change to methylphenidate transdermal patch",
+                                "Use Methylphenidate extended release and consider adding Methylphenidate immediate release after school for later day coverage",
+                                "Atomoxetine",
+                                "No labeling change warns of potential severe liver injury and Black box warning about increased risk of suicidal ideation in children and adolescents" };
+
+    private string QuestionNo = "Question No ";
+    private string yourAnswer = "Your Answer is";
+    private string _TypeText;
+    private SpeechToText _service;
+
+    void Start()
+    {
+        final = false;
+        index = 0;
+        LogSystem.InstallDefaultReactors();
+        Runnable.Run(CreateService());
+        StartCoroutine(QuesNo());
+    }
+
+    private void Update()
+    {
+        if (final)
+        {
+            StartRecording();
+            if (IsAnswer)
+            {
+                ResultsField.text = text;
+                text = text.Remove(text.Length-1);
+
+                CorrectAns.enabled = true;
+                AnswerCase.enabled = true;
+
+                CorrectAns.text = "The Correct Answer is " + Answers[index];
+
+                Answers[index] = Answers[index].ToLower();
+
+                Debug.LogWarning((CalculateSimilarity(text, Answers[index]) * 100));
+               
+                if (string.Compare(text, Answers[index]) == 0)
+                {
+                    AnswerCase.text = "Your Answer is Correct";
+                }
+                else
+                {
+                    AnswerCase.text = "Your Answer is Wrong";
+                }
+
+                IsAnswer = false;
+                final = false;
+
+                index++;
+
+                if (index < Questions.Length)
+                {
+                    StartCoroutine(StartAgain());
+                }
+            }
+        }
+        else
+        {
+            StopRecording();
+        }
+       
+    }
+
+    IEnumerator StartAgain()
+    {
+        yield return delay;
+        StartCoroutine(QuesNo());
+    }
+
+    IEnumerator QuesNo()
+    {
+        QuestionNo = "Question No "+(index+1);
+
+        qesNO.text = "";
+        questionTxt.text = "";
+        ResultsField.text = "";
+        CorrectAns.enabled = false;
+        AnswerCase.enabled = false;
+
+        foreach (char letter in QuestionNo.ToCharArray())
+        {
+            _TypeText += letter;
+            qesNO.text = _TypeText;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        _TypeText = "";
+        StartCoroutine(Question());
+    }
+
+    IEnumerator Question()
+    {
+
+        foreach (char letter in Questions[index].ToCharArray())
+        {
+            _TypeText += letter;
+            questionTxt.text = _TypeText;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        _TypeText = "";
+        StartCoroutine(YourAnswer());
+    }
+
+    IEnumerator YourAnswer()
+    {
+
+        foreach (char letter in yourAnswer.ToCharArray())
+        {
+            _TypeText += letter;
+            ResultsField.text = _TypeText;
+            yield return new WaitForSeconds(textSpeed);
+        }
+
+        _TypeText = "";
+        final = true;
+    }
+
+    private IEnumerator CreateService()
+    {
+        if (string.IsNullOrEmpty(_iamApikey))
+        {
+            throw new WatsonException("Plesae provide IAM ApiKey for the service.");
+        }
+
+        //  Create credential and instantiate service
+        Credentials credentials = null;
+
+        //  Authenticate using iamApikey
+        TokenOptions tokenOptions = new TokenOptions()
+        {
+            IamApiKey = _iamApikey
+        };
+
+        credentials = new Credentials(tokenOptions, _serviceUrl);
+
+        //  Wait for tokendata
+        while (!credentials.HasIamTokenData())
+            yield return null;
+
+        _service = new SpeechToText(credentials);
+        _service.StreamMultipart = true;
+
+        Active = true;
+       // StopRecording();
+
+    }
+
+    public bool Active
+    {
+        get { return _service.IsListening; }
+        set
+        {
+            if (value && !_service.IsListening)
+            {
+                _service.RecognizeModel = (string.IsNullOrEmpty(_recognizeModel) ? "en-US_BroadbandModel" : _recognizeModel);
+                _service.DetectSilence = true;
+                _service.EnableWordConfidence = true;
+                _service.EnableTimestamps = true;
+                _service.SilenceThreshold = 0.01f;
+                _service.MaxAlternatives = 0;
+                _service.EnableInterimResults = true;
+                _service.OnError = OnError;
+                _service.InactivityTimeout = -1;
+                _service.ProfanityFilter = false;
+                _service.SmartFormatting = true;
+                _service.SpeakerLabels = false;
+                _service.WordAlternativesThreshold = null;
+                _service.StartListening(OnRecognize, OnRecognizeSpeaker);
+            }
+            else if (!value && _service.IsListening)
+            {
+                _service.StopListening();
+            }
+        }
+    }
+
+    private void StartRecording()
+    {
+        if (_recordingRoutine == 0)
+        {
+            UnityObjectUtil.StartDestroyQueue();
+            _recordingRoutine = Runnable.Run(RecordingHandler());
+        }
+    }
+
+    private void StopRecording()
+    {
+        if (_recordingRoutine != 0)
+        {
+            Microphone.End(_microphoneID);
+            Runnable.Stop(_recordingRoutine);
+            _recordingRoutine = 0;
+        }
+    }
+
+    private void OnError(string error)
+    {
+        Active = false;
+
+        Log.Debug("ExampleStreaming.OnError()", "Error! {0}", error);
+    }
+
+    private IEnumerator RecordingHandler()
+    {
+        Log.Debug("ExampleStreaming.RecordingHandler()", "devices: {0}", Microphone.devices);
+        _recording = Microphone.Start(_microphoneID, true, _recordingBufferSize, _recordingHZ);
+        yield return null;      // let _recordingRoutine get set..
+
+        if (_recording == null)
+        {
+            StopRecording();
+            yield break;
+        }
+
+        bool bFirstBlock = true;
+        int midPoint = _recording.samples / 2;
+        float[] samples = null;
+
+        while (_recordingRoutine != 0 && _recording != null)
+        {
+            int writePos = Microphone.GetPosition(_microphoneID);
+            if (writePos > _recording.samples || !Microphone.IsRecording(_microphoneID))
+            {
+                Log.Error("ExampleStreaming.RecordingHandler()", "Microphone disconnected.");
+
+                StopRecording();
+                yield break;
+            }
+
+            if ((bFirstBlock && writePos >= midPoint)
+              || (!bFirstBlock && writePos < midPoint))
+            {
+                // front block is recorded, make a RecordClip and pass it onto our callback.
+                samples = new float[midPoint];
+                _recording.GetData(samples, bFirstBlock ? 0 : midPoint);
+
+                AudioData record = new AudioData();
+				record.MaxLevel = Mathf.Max(Mathf.Abs(Mathf.Min(samples)), Mathf.Max(samples));
+                record.Clip = AudioClip.Create("Recording", midPoint, _recording.channels, _recordingHZ, false);
+                record.Clip.SetData(samples, 0);
+
+                _service.OnListen(record);
+
+                bFirstBlock = !bFirstBlock;
+            }
+            else
+            {
+                // calculate the number of samples remaining until we ready for a block of audio, 
+                // and wait that amount of time it will take to record.
+                int remaining = bFirstBlock ? (midPoint - writePos) : (_recording.samples - writePos);
+                float timeRemaining = (float)remaining / (float)_recordingHZ;
+
+                yield return new WaitForSeconds(timeRemaining);
+            }
+
+        }
+
+        yield break;
+    }
+
+    private void OnRecognize(SpeechRecognitionEvent result, Dictionary<string, object> customData)
+    {
+        if (result != null && result.results.Length > 0)
+        {
+            foreach (var res in result.results)
+            {
+                foreach (var alt in res.alternatives)
+                {
+                    text = alt.transcript;//string.Format("{0} ({1}, {2:0.00})\n", alt.transcript, res.final ? "Final" : "Interim", alt.confidence);
+                    //Log.Debug("ExampleStreaming.OnRecognize()", text);
+                   // ResultsField.text = text;
+                }
+
+                if (res.keywords_result != null && res.keywords_result.keyword != null)
+                {
+                    foreach (var keyword in res.keywords_result.keyword)
+                    {
+                        Log.Debug("ExampleStreaming.OnRecognize()", "keyword: {0}, confidence: {1}, start time: {2}, end time: {3}", keyword.normalized_text, keyword.confidence, keyword.start_time, keyword.end_time);
+                    }
+                }
+
+                if (res.word_alternatives != null)
+                {
+                    foreach (var wordAlternative in res.word_alternatives)
+                    {
+                        Log.Debug("ExampleStreaming.OnRecognize()", "Word alternatives found. Start time: {0} | EndTime: {1}", wordAlternative.start_time, wordAlternative.end_time);
+                        foreach(var alternative in wordAlternative.alternatives)
+                            Log.Debug("ExampleStreaming.OnRecognize()", "\t word: {0} | confidence: {1}", alternative.word, alternative.confidence);
+                    }
+                }
+            }
+        }
+    }
+
+    private void OnRecognizeSpeaker(SpeakerRecognitionEvent result, Dictionary<string, object> customData)
+    {
+        if (result != null)
+        {
+            foreach (SpeakerLabelsResult labelResult in result.speaker_labels)
+            {
+                Log.Debug("ExampleStreaming.OnRecognize()", string.Format("speaker result: {0} | confidence: {3} | from: {1} | to: {2}", labelResult.speaker, labelResult.from, labelResult.to, labelResult.confidence));
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Returns the number of steps required to transform the source string
+    /// into the target string.
+    /// </summary>
+    int ComputeLevenshteinDistance(string source, string target)
+    {
+        if ((source == null) || (target == null)) return 0;
+        if ((source.Length == 0) || (target.Length == 0)) return 0;
+        if (source == target) return source.Length;
+
+        int sourceWordCount = source.Length;
+        int targetWordCount = target.Length;
+
+        // Step 1
+        if (sourceWordCount == 0)
+            return targetWordCount;
+
+        if (targetWordCount == 0)
+            return sourceWordCount;
+
+        int[,] distance = new int[sourceWordCount + 1, targetWordCount + 1];
+
+        // Step 2
+        for (int i = 0; i <= sourceWordCount; distance[i, 0] = i++) ;
+        for (int j = 0; j <= targetWordCount; distance[0, j] = j++) ;
+
+        for (int i = 1; i <= sourceWordCount; i++)
+        {
+            for (int j = 1; j <= targetWordCount; j++)
+            {
+                // Step 3
+                int cost = (target[j - 1] == source[i - 1]) ? 0 : 1;
+                
+                // Step 4
+                distance[i, j] = Mathf.Min(Mathf.Min(distance[i - 1, j] + 1, distance[i, j - 1] + 1), distance[i - 1, j - 1] + cost);
+            }
+        }
+
+        return distance[sourceWordCount, targetWordCount];
+    }
+
+    /// <summary>
+    /// Calculate percentage similarity of two strings
+    /// <param name="source">Source String to Compare with</param>
+    /// <param name="target">Targeted String to Compare</param>
+    /// <returns>Return Similarity between two strings from 0 to 1.0</returns>
+    /// </summary>
+    double CalculateSimilarity(string source, string target)
+    {
+        if ((source == null) || (target == null)) return 0.0;
+        if ((source.Length == 0) || (target.Length == 0)) return 0.0;
+        if (source == target) return 1.0;
+
+        int stepsToSame = ComputeLevenshteinDistance(source, target);
+        return (1.0 - ((double)stepsToSame / (double)Mathf.Max(source.Length, target.Length)));
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Question-Answer-Voice-Recognition
+
+-Library 
+    
+    IBM Watson For Unity 
+
+- Tools
+
+    Unity2018.3.0f1
